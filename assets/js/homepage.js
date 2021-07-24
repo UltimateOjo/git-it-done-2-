98be57c39a139f79ed5eadc6e0efb2b15c0b0571
Gbenga <olugbengaojo97@gmail.com>
2021-07-23 21:04:50 -0500

    finished

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,5 @@
 var userFormEl = document.querySelector("#user-form");
+var languageButtonsEl = document.querySelector("#language-buttons");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
@@ -21,38 +22,64 @@ var formSubmitHandler = function(event) {
   }
 };
 
+var buttonClickHandler = function(event) {
+  // get the language attribute from the clicked element
+  var language = event.target.getAttribute("data-language");
+
+  if (language) {
+    getFeaturedRepos(language);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
+};
+
 var getUserRepos = function(user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-// make a get request to url
-fetch(apiUrl).then(function(response) {
+  // make a get request to url
+  fetch(apiUrl)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function(data) {
+          console.log(data);
+          displayRepos(data, user);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function(error) {
+      alert("Unable to connect to GitHub");
+    });
+};
+
+var getFeaturedRepos = function(language) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
     // request was successful
     if (response.ok) {
       response.json().then(function(data) {
-        displayIssues(data);
-  
-        // check if api has paginated issues
-        if (response.headers.get("Link")) {
-          displayWarning(repo);
-        }
+        displayRepos(data.items, language);
       });
     } else {
-      // if not successful, redirect to homepage
-      document.location.replace("./index.html");
+      alert("Error: " + response.statusText);
     }
   });
-}
+};
+
 var displayRepos = function(repos, searchTerm) {
   // check if api returned any repos
   if (repos.length === 0) {
     repoContainerEl.textContent = "No repositories found.";
     return;
   }
-    // create a link for each repo
-    var repoEl = document.createElement("a");
-    repoEl.classList = "list-item flex-row justify-space-between align-center";
-    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
   repoSearchTerm.textContent = searchTerm;
 
@@ -60,11 +87,12 @@ var displayRepos = function(repos, searchTerm) {
   for (var i = 0; i < repos.length; i++) {
     // format repo name
     var repoName = repos[i].owner.login + "/" + repos[i].name;
-  
-    // create a container for each repo
+
+    // create a link for each repo
     var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
-    repoEl.setAttribute("href", "./single-repo.html");
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+
     // create a span element to hold repository name
     var titleEl = document.createElement("span");
     titleEl.textContent = repoName;
@@ -92,21 +120,6 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
-// add event listeners to forms
+// add event listeners to form and button container
 userFormEl.addEventListener("submit", formSubmitHandler);
-
-var getRepoName = function() {
-    // grab repo name from url query string
-    var queryString = document.location.search;
-    var repoName = queryString.split("=")[1];
-  
-    if (repoName) {
-      // display repo name on the page
-      repoNameEl.textContent = repoName;
-  
-      getRepoIssues(repoName);
-    } else {
-      // if no repo was given, redirect to the homepage
-      document.location.replace("./index.html");
-    }
-  };
+languageButtonsEl.addEventListener("click", buttonClickHandler);
